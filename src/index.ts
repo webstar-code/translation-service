ab80b22ed9c07c6f3a5cd01e38f9756dfeb34d16
webstar-code <bhaveshrc2001@gmail.com>
2024-08-02 11:19:56 +0530

    fix

--- src/index.ts
+++ src/index.ts
@@ -13,142 +13,140 @@ const server = http.createServer(app);
 app.use(express.json());
 
 app.use(cors())
+app.use(langPoc);
 
-connectToDatabase().then(() => {
-  const io = new Server(server, {
-    cors: {
-      origin: "http://localhost:5173",
-      methods: ["GET", "POST"]
-    }
-  });
+// connectToDatabase().then(() => {
+//   // const io = new Server(server, {
+//   //   cors: {
+//   //     origin: "http://localhost:5173",
+//   //     methods: ["GET", "POST"]
+//   //   }
+//   // });
 
-  // handle the chat sessions table
-  io.use(async (socket, next) => {
-    const userId = socket.handshake.auth.userId;
-    if (!userId) {
-      return next(new Error("invalid username"));
-    }
-    // @ts-ignore
-    socket.userId = userId;
-    await updateChatSession({ socketId: socket.id, userId: userId, lastSeen: new Date(), status: 'online' })
-    next();
-  });
+//   // // handle the chat sessions table
+//   // io.use(async (socket, next) => {
+//   //   const userId = socket.handshake.auth.userId;
+//   //   if (!userId) {
+//   //     return next(new Error("invalid username"));
+//   //   }
+//   //   // @ts-ignore
+//   //   socket.userId = userId;
+//   //   await updateChatSession({ socketId: socket.id, userId: userId, lastSeen: new Date(), status: 'online' })
+//   //   next();
+//   // });
 
-  io.on('connection', (socket) => {
+//   // io.on('connection', (socket) => {
 
-    socket.on("msg", async ({ content, to }) => {
-      console.log(content, to);
-      const result = await chatSessions.findOne({ userId: to });
+//   //   socket.on("msg", async ({ content, to }) => {
+//   //     console.log(content, to);
+//   //     const result = await chatSessions.findOne({ userId: to });
 
-      if (result && result.status === 'online') {
-        socket.emit("msg", {
-          content,
-          createdAt: new Date(),
-          receiverId: to,
-          // @ts-ignore
-          senderId: socket.userId,
-          status: 'delivered',
-          read: true
-        });
-        socket.to(result.socketId).emit("msg", {
-          content,
-          createdAt: new Date(),
-          receiverId: to,
-          // @ts-ignore
-          senderId: socket.userId,
-          status: 'delivered'
-        });
-        storeMessage({
-          content,
-          createdAt: new Date(),
-          receiverId: to,
-          // @ts-ignore
-          senderId: socket.userId,
-          status: 'delivered',
-          read: false
-        })
-      } else {
-        // Send Notification
-        // Read or not
-        // Get no of unread messages
-        // Create a queue of pending messages, when user id connects send messages to that socket
-        socket.emit("msg", {
-          content,
-          createdAt: new Date(),
-          receiverId: to,
-          // @ts-ignore
-          senderId: socket.userId,
-          status: 'pending'
-        });
-        storeMessage({
-          content,
-          createdAt: new Date(),
-          receiverId: to,
-          // @ts-ignore
-          senderId: socket.userId,
-          status: 'pending',
-          read: false
+//   //     if (result && result.status === 'online') {
+//   //       socket.emit("msg", {
+//   //         content,
+//   //         createdAt: new Date(),
+//   //         receiverId: to,
+//   //         // @ts-ignore
+//   //         senderId: socket.userId,
+//   //         status: 'delivered',
+//   //         read: true
+//   //       });
+//   //       socket.to(result.socketId).emit("msg", {
+//   //         content,
+//   //         createdAt: new Date(),
+//   //         receiverId: to,
+//   //         // @ts-ignore
+//   //         senderId: socket.userId,
+//   //         status: 'delivered'
+//   //       });
+//   //       storeMessage({
+//   //         content,
+//   //         createdAt: new Date(),
+//   //         receiverId: to,
+//   //         // @ts-ignore
+//   //         senderId: socket.userId,
+//   //         status: 'delivered',
+//   //         read: false
+//   //       })
+//   //     } else {
+//   //       // Send Notification
+//   //       // Read or not
+//   //       // Get no of unread messages
+//   //       // Create a queue of pending messages, when user id connects send messages to that socket
+//   //       socket.emit("msg", {
+//   //         content,
+//   //         createdAt: new Date(),
+//   //         receiverId: to,
+//   //         // @ts-ignore
+//   //         senderId: socket.userId,
+//   //         status: 'pending'
+//   //       });
+//   //       storeMessage({
+//   //         content,
+//   //         createdAt: new Date(),
+//   //         receiverId: to,
+//   //         // @ts-ignore
+//   //         senderId: socket.userId,
+//   //         status: 'pending',
+//   //         read: false
 
-        })
-      }
-
-
-    });
-
-    socket.on('disconnect', async () => {
-      // @ts-ignore
-      await updateChatSession({ socketId: null, userId: socket.userId, lastSeen: new Date(), status: 'offline' })
-      console.log('user disconnected');
-    });
-  });
-
-  app.use(responseTime(function (req: any, res: any, time: any) {
-    var stat = (req.method + req.url).toLowerCase()
-      .replace(/[:.]/g, '')
-      .replace(/\//g, '_')
-    console.log(stat, time)
-  }))
-  app.use(langPoc);
-
-  app.get("/", (req, res) => {
-    res.send("Hello from 3000").status(200);
-  })
+//   //       })
+//   //     }
 
 
-  app.get("/user-chat-session", async (req, res) => {
-    const { id } = req.query;
-    const result = await chatSessions.findOne({ userId: id });
-    if (result) {
-      res.json(result).status(200);
-    } else {
-      res.json({ message: "Server Error" }).status(200);
-    }
-  })
+//   //   });
 
-  app.get("/user-chat-messages", async (req, res) => {
-    const { senderId, receiverId } = req.query;
-    console.log(senderId, receiverId);
-    const result = await messages.find({ senderId, receiverId }).toArray();
-    if (result) {
-      res.json(result).status(200);
-    } else {
-      res.json({ message: "Server Error" }).status(200);
-    }
-  })
+//   //   socket.on('disconnect', async () => {
+//   //     // @ts-ignore
+//   //     await updateChatSession({ socketId: null, userId: socket.userId, lastSeen: new Date(), status: 'offline' })
+//   //     console.log('user disconnected');
+//   //   });
+//   // });
 
-  app.get("/conversation", async (req, res) => {
-    const { user1, user2 } = req.query;
-    const c = await conversations.findOne({ users: { $all: [user1, user2] } });
-    if (c) {
-      const result = await messages.find({ conversationId: c._id }).toArray();
-      res.json(result).status(200);
-    } else {
-      res.json({ message: "No Conversation Found." }).status(200);
-    }
-  })
-})
+//   // app.use(responseTime(function (req: any, res: any, time: any) {
+//   //   var stat = (req.method + req.url).toLowerCase()
+//   //     .replace(/[:.]/g, '')
+//   //     .replace(/\//g, '_')
+//   //   console.log(stat, time)
+//   // }))
+
+//   app.get("/", (req, res) => {
+//     res.send("Hello from 3000").status(200);
+//   })
 
 
+//   // app.get("/user-chat-session", async (req, res) => {
+//   //   const { id } = req.query;
+//   //   const result = await chatSessions.findOne({ userId: id });
+//   //   if (result) {
+//   //     res.json(result).status(200);
+//   //   } else {
+//   //     res.json({ message: "Server Error" }).status(200);
+//   //   }
+//   // })
+
+//   app.get("/user-chat-messages", async (req, res) => {
+//     const { senderId, receiverId } = req.query;
+//     console.log(senderId, receiverId);
+//     const result = await messages.find({ senderId, receiverId }).toArray();
+//     if (result) {
+//       res.json(result).status(200);
+//     } else {
+//       res.json({ message: "Server Error" }).status(200);
+//     }
+//   })
+
+//   app.get("/conversation", async (req, res) => {
+//     const { user1, user2 } = req.query;
+//     const c = await conversations.findOne({ users: { $all: [user1, user2] } });
+//     if (c) {
+//       const result = await messages.find({ conversationId: c._id }).toArray();
+//       res.json(result).status(200);
+//     } else {
+//       res.json({ message: "No Conversation Found." }).status(200);
+//     }
+//   })
+// })
 
 const port = process.env.PORT || 3000;
 
